8fc1578a5afd345f2eba2b9e62c79264560095d6
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-08 18:08:21 +0530

    init

---
+++ B.java
@@ -0,0 +1,19 @@
+package edu;
+public class B{
+     int x,y,z;
+     public B(){
+
+       x=88;
+	y=8;
+System.out.println("this is b construcyor");
+
+}
+public void sub()
+{
+z=x-y;
+System.out.println("the substracyion z is="+z);
+
+}
+
+
+}

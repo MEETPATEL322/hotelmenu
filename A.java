9400c8095a0cdfa60b64a226f4b1b368324ec10f
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-08 18:03:58 +0530

    hello

---
+++ A.java
@@ -0,0 +1,18 @@
+package org;
+ 
+public class A{
+           int x,y,z;
+         public A(){
+     		x=88;
+		y=8;
+		System.out.println("this is constructor a");
+
+}
+    public void add(){
+
+          z=x+y;
+          System.out.println("the additon of z is ="+z);
+}
+
+
+}

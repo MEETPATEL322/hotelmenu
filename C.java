9dbfad172824ba8ac58db2fa8fca1fe32b933928
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-08 18:15:44 +0530

    hmm

---
+++ C.java
@@ -0,0 +1,17 @@
+package com;
+public class C{
+     int x,y,z;
+    public C(){
+    x=88;
+y=8;
+System.out.println("this is constructor b");
+
+}
+public void mult()
+{
+z=x*y;
+System.out.println("ans of multiplication z is="+z);
+}
+
+
+}
